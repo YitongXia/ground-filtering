9a62de0afc01e522958ebfdce92faabac40c9e10
F.Z <zmocheng@gmail.com>
2022-01-13 19:30:34 +0100

    Update GroundFilter.h
    Add title information and macro definitions.

--- src/GroundFilter.h
+++ src/GroundFilter.h
@@ -1,12 +1,19 @@
-/*
-  GEO1015.2021
-  hw03 
-  --
-  [YOUR NAME] 
-  [YOUR STUDENT NUMBER] 
-  [YOUR NAME] 
-  [YOUR STUDENT NUMBER] 
-*/
+/********************************************************************
+* GEO1015.2021
+* hw03
+* Yitong Xia
+* 5445825
+* Fengyan Zhang
+* 5462150
+*
+* The idea of CSF is from this paper:
+* http://www.mdpi.com/2072-4292/8/6/501/htm
+* The basic architectures and ideas are inspired by this article (its related code is open source).
+*
+*********************************************************************/
+
+#ifndef _GROUND_FILTER_H_
+#define _GROUND_FILTER_H_
 
 #include <string>
 #include <vector>
@@ -27,3 +34,5 @@ void write_lasfile(const std::string filename, const std::vector<Point>& pointcl
 
 void groundfilter_tin(const std::vector<Point>& pointcloud, const json& jparams);
 void groundfilter_csf(const std::vector<Point>& pointcloud, const json& jparams);
+
+#endif
